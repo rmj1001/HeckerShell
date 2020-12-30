6837cb9ae03bed900b86e734edef356d61a55a10
Nidia Achrys <rmichael1001@gmail.com>
2020-12-30 14:08:16 -0500

    api function naming better

--- scripts/libs/01-api.sh
+++ scripts/libs/01-api.sh
@@ -31,39 +31,39 @@ class "api"
 
 # Description: Retrieve the name of the script file
 #
-# Usage: name=`api.SCRIPTNAME`
-api.SCRIPTNAME ()
+# Usage: name=`api.script.name`
+api.script.name ()
 {
 	local absolute="`readlink -f ${0}`"
 	
-	api.PRINT "${absolute##*/}"
+	api.std.printLn "${absolute##*/}"
 	return 0
 }
 
 # Description: Retrieve the name of the directory the script file is run in
 #
-# Usage: parent=`api.SCRIPTDIR`
-api.SCRIPTDIR ()
+# Usage: parent=`api.script.directory`
+api.script.directory ()
 {
 	local absolute="`readlink -f ${0}`"
 
-	api.PRINT "$( dirname ${absolute} )"
+	api.std.printLn "$( dirname ${absolute} )"
 	return 0
 }
 
 # Description: Prints the script name and version, as well as copyright, 
 # license notice, and author name
 #
-# Usage: api.VERSION
-api.VERSION ()
+# Usage: api.script.version
+api.script.version ()
 {
-	api.PRINT "`api.SCRIPTNAME` v${version:=1.0}"
-	api.PRINT "Copyright (C) `date +"%Y"` ${author:=Some Random Scripter}"
-	api.PRINT "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>."
-	api.PRINT "This is free software: you are free to change and redistribute it."
-	api.PRINT "There is NO WARRANTY, to the extent permitted by law."
-	api.PRINT
-	api.PRINT "Written by ${author:=Some Random Scripter}."
+	api.std.printLn "`api.script.name` v${version:=1.0}"
+	api.std.printLn "Copyright (C) `date +"%Y"` ${author:=Some Random Scripter}"
+	api.std.printLn "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>."
+	api.std.printLn "This is free software: you are free to change and redistribute it."
+	api.std.printLn "There is NO WARRANTY, to the extent permitted by law."
+	api.std.printLn
+	api.std.printLn "Written by ${author:=Some Random Scripter}."
 	return 0
 }
 
@@ -71,8 +71,8 @@ api.VERSION ()
 
 # Description: Replacement for 'echo'
 #
-# Usage: api.PRINT "text"
-api.PRINT ()
+# Usage: api.std.printLn "text"
+api.std.printLn ()
 {
 	if [ -z "${1}" ]; then
 		printf "\n"
@@ -85,8 +85,8 @@ api.PRINT ()
 
 # Description: Pauses script execution until the user presses ENTER
 #
-# Usage: api.PAUSE
-api.PAUSE ()
+# Usage: api.std.pause
+api.std.pause ()
 {
 	read -p "Press ENTER to continue..." cmd
 	return 0
@@ -94,34 +94,34 @@ api.PAUSE ()
 
 # Description: Send a log message to the terminal
 #
-# Usage: api.LOG "api.This is awesome!"
-api.LOG ()
+# Usage: api.std.logMsg "api.This is awesome!"
+api.std.logMsg ()
 {
-	api.PRINT "`api.SCRIPTNAME`: ${@}"
+	api.std.printLn "`api.script.name`: ${@}"
 }
 
 # Description: Send an error message and give a return code of 1
 #
-# Usage: api.ERROR "api.This broke!"
-api.ERROR ()
+# Usage: api.std.errorMsg "api.This broke!"
+api.std.errorMsg ()
 {
-	api.LOG "${@}" && return 1
+	api.std.logMsg "${@}" && return 1
 }
 
 # Description: Send an error message, exit, and give a return code of 1
 #
-# Usage: api.FAIL "This script broke!"
-api.FAIL ()
+# Usage: api.std.failMsg "This script broke!"
+api.std.failMsg ()
 {
-	api.LOG "${@}" && exit 1
+	api.std.logMsg "${@}" && exit 1
 }
 
 ###################### STRING MANIPULATION ######################
 
-# Description: Converts a string to all api.FAIL characters
+# Description: Converts a string to all api.std.failMsg characters
 #
-# Usage: name=`api.LOWERCASE $name`
-api.LOWERCASE ()
+# Usage: name=`api.string.toLowerCase $name`
+api.string.toLowerCase ()
 {
 	printf "${1}" | tr "[:upper:]" "[:lower:]"
 	return 0
@@ -129,8 +129,8 @@ api.LOWERCASE ()
 
 # Description: Converts a string to all UPPERCASE characters
 #
-# Usage: name=`api.UPPERCASE $name`
-api.UPPERCASE ()
+# Usage: name=`api.string.toUpperCase $name`
+api.string.toUpperCase ()
 {
 	printf "${1}" | tr "[:lower:]" "[:upper:]"
 	return 0
@@ -138,8 +138,8 @@ api.UPPERCASE ()
 
 # Description: Trim all leading/trailing whitespace from a string
 #
-# Usage: api.TRIM "   this      "
-api.TRIM ()
+# Usage: api.string.trim "   this      "
+api.string.trim ()
 {
     local var="$*"
     # remove leading whitespace characters
@@ -153,36 +153,36 @@ api.TRIM ()
 
 # Description: Error message for when an invalid command is used
 #
-# Usage: api.INVALID "hlep"
-api.INVALID ()
+# Usage: api.errors.invalidCommand "hlep"
+api.errors.invalidCommand ()
 {
-	api.FAIL "Invalid command \"${1}\". See \"`api.SCRIPTNAME` help\"."
+	api.std.failMsg "Invalid command \"${1}\". See \"`api.script.name` help\"."
 }
 
 # Description: Error message for when no command is used
 #
-# Usage: api.EMPTYCMD
-api.EMPTYCMD ()
+# Usage: api.errors.emptyCommand
+api.errors.emptyCommand ()
 { 
-	api.FAIL "You must specify a subcommand. See \"`api.SCRIPTNAME` help\"."
+	api.std.failMsg "You must specify a subcommand. See \"`api.script.name` help\"."
 }
 
 # Description: Checks for a filename in $PATH (commands), if not found then exit with an error
 #
-# Usage: api.DEPENDENCY "7z"
-api.DEPENDENCY ()
+# Usage: api.required.command "7z"
+api.required.command ()
 {
-	[[ ! `command -v ${1}` ]] && api.FAIL "'${1}' is required to run this program."
+	[[ ! `command -v ${1}` ]] && api.std.failMsg "'${1}' is required to run this program."
 }
 
 # Description: Checks to see if the script is being run as root, and if not then exit.
 #
-# Usage: api.ROOT
-api.ROOT ()
+# Usage: api.required.root
+api.required.root ()
 {
 	if [[ $EUID -ne 0 ]]
 	then
-		api.FAIL "This script must be run as root"
+		api.std.failMsg "This script must be run as root"
 	fi
 }
 
@@ -191,10 +191,10 @@ api.ROOT ()
 # Description: Sources all script files from a directory
 # under $SCRIPTS/libs
 #
-# Usage: api.IMPORT "libraryName"
-api.IMPORT ()
+# Usage: api.lib.import "libraryName"
+api.lib.import ()
 {
-	local libname="`api.LOWERCASE ${1}`"
+	local libname="`api.string.toLowerCase ${1}`"
 	local dir="${SCRIPTS}/libs/${libname}"
 
 	if [ -d "${dir}" ]
@@ -209,7 +209,7 @@ api.IMPORT ()
 	
 	else
 
-		api.FAIL "Invalid library '${libname}'"
+		api.std.failMsg "Invalid library '${libname}'"
 	
 	fi
 }
