ec87bd86bbaa4519c875ef11915c72dc1e3c9849
Roy Conn <rmichael1001@gmail.com>
2021-03-03 18:06:00 -0500

    Fixed various scripts

---
+++ scripts/libs/bash-functions.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+##############################################
+#	Author: Nidia Achrys
+#	Library: api.sh
+#	Version: 1.0
+# 
+# 	Description:
+#   	This file provides common bash functions
+#		that I use in all my bash scripts. Feel free
+#		to use these functions in your own scripts.
+##############################################
+
+###################### INTERACTIVITY ######################
+
+# Description: Replacement for 'echo'
+#
+# Usage: PRINT "text"
+PRINT()
+{
+	printf "%b\n" "${@}"
+}
+
+# Description: Pauses script execution until the user presses ENTER
+#
+# Usage: PAUSE
+PAUSE ()
+{
+	local p=""
+	read -p "Press <ENTER> to continue..." p
+
+	return 0
+}
+
+# Description: Sets the terminal window title
+#
+# Usage: TITLE "test"
+TITLE ()
+{
+	printf "\033]2;${1}\a"
+}
+
+# Description: Generate a random number from 1 to the specified maximum
+#
+# Usage: RANDOM_NUM 100
+RANDOM_NUM ()
+{
+	printf "%b" "$(( ${RANDOM} % ${1} + 1 ))"
+}
+
+###################### STRING MANIPULATION ######################
+
+# Description: Converts a string to all api.std.failMsg characters
+#
+# Usage: name="$(LOWERCASE $name)"
+LOWERCASE ()
+{
+	printf "${1}" | tr "[:upper:]" "[:lower:]"
+	return 0
+}
+
+# Description: Converts a string to all UPPERCASE characters
+#
+# Usage: name="$(UPPERCASE $name)"
+UPPERCASE ()
+{
+	printf "${1}" | tr "[:lower:]" "[:upper:]"
+	return 0
+}
+
+# Description: Trim all leading/trailing whitespace from a string
+#
+# Usage: TRIM "   this      "
+TRIM ()
+{
+    local var="$*"
+
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    
+	# remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    printf '%s' "$var"
+}
+
+###################### SCRIPT PREPROCCESSORS & PERMISSION CHECKING ######################
+
+# Description: Checks for a filename in $PATH (commands), if not found then exit with an error
+#
+# Usage: REQUIRE_CMD "7z" "tar"
+REQUIRE_CMD ()
+{
+	for arg in "${@}"; do
+		if [[ ! -x "$(which $arg)" ]]; then
+
+			printf "%b\n" "'${arg}' is required to run this program!"
+			exit 1
+
+		fi
+	done
+}
+
+# Description: Checks to see if the script is being run as root, and if not then exit.
+#
+# Usage: REQUIRE_ROOT
+REQUIRE_ROOT ()
+{
+	if [[ $EUID -ne 0 ]]
+	then
+		printf "%b\n" "This script must be run as root"
+	fi
+}
