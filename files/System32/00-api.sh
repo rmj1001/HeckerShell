2ac9c0ff78650941b9364c7a270a216be0b7e0d6
Roy Conn <rmichael1001@gmail.com>
2021-09-16 11:49:21 -0400

    scripts use API lib again

--- files/System32/00-api.sh
+++ files/System32/00-api.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ##############################################
-#	Author: Nidia Achrys
+#	Author: Dark Lord Meowron
 #	Library: api.sh
 #	Version: 1.0
-# 
+#
 # 	Description:
 #   	This file provides common bash functions
 #		that I use in all my bash scripts. Feel free
@@ -14,24 +14,24 @@
 # Description: Replacement for 'echo'
 #
 # Usage: PRINT "text"
-PRINT()
-{
+# Returns: string
+PRINT() {
 	printf "%b\n" "${@}"
 }
 
 # Description: 'echo' replacement w/o newline
 #
 # Usage: NPRINT "text"
-NPRINT()
-{
+# Returns: string
+NPRINT() {
 	printf "%b" "${@}"
 }
 
 # Description: Pauses script execution until the user presses ENTER
 #
 # Usage: PAUSE
-PAUSE ()
-{
+# Returns: int
+PAUSE() {
 	read -r -p "Press <ENTER> to continue..."
 
 	return 0
@@ -40,24 +40,24 @@ PAUSE ()
 # Description: Sets the terminal window title
 #
 # Usage: TITLE "test"
-TITLE ()
-{
+# Returns: void
+TITLE() {
 	printf "%b" "\033]2;${1}\a"
 }
 
 # Description: Generate a random number from 1 to the specified maximum
 #
 # Usage: RANDOM_NUM 100
-RANDOM_NUM ()
-{
-	printf "%b" "$(( RANDOM % ${1} + 1 ))"
+# Returns: int
+RANDOM_NUM() {
+	printf "%b" "$((RANDOM % ${1} + 1))"
 }
 
 # Description: Converts a string to all api.std.failMsg characters
 #
 # Usage: name="$(LOWERCASE $name)"
-LOWERCASE ()
-{
+# Returns: string
+LOWERCASE() {
 	printf "%b" "${1}" | tr "[:upper:]" "[:lower:]"
 	return 0
 }
@@ -65,8 +65,8 @@ LOWERCASE ()
 # Description: Converts a string to all UPPERCASE characters
 #
 # Usage: name="$(UPPERCASE $name)"
-UPPERCASE ()
-{
+# Returns: string
+UPPERCASE() {
 	printf "%b" "${1}" | tr "[:lower:]" "[:upper:]"
 	return 0
 }
@@ -74,25 +74,25 @@ UPPERCASE ()
 # Description: Trim all leading/trailing whitespace from a string
 #
 # Usage: TRIM "   this      "
-TRIM ()
-{
-    local var="$*"
+# Returns: string
+TRIM() {
+	local var="$*"
 
-    # remove leading whitespace characters
-    var="${var##*( )}"
-    
+	# remove leading whitespace characters
+	var="${var##*( )}"
+
 	# remove trailing whitespace characters
-    var="${var%%*( )}"
-    
-    # Return trimmed string
-    printf '%s' "$var"
+	var="${var%%*( )}"
+
+	# Return trimmed string
+	printf '%s' "$var"
 }
 
 # Description: Checks for a filename in $PATH (commands), if not found then exit with an error
 #
 # Usage: REQUIRE_CMD "7z" "tar"
-REQUIRE_CMD ()
-{
+# Returns: string
+REQUIRE_CMD() {
 	NEEDED=()
 
 	for arg in "${@}"; do
@@ -106,7 +106,7 @@ REQUIRE_CMD ()
 	if [[ ${#NEEDED[@]} -gt 0 ]]; then
 		printf "%b\n" "The following programs are required to run this program:"
 		printf "%b\n" "${NEEDED[@]}"
-		
+
 		exit 1
 	fi
 }
@@ -114,11 +114,43 @@ REQUIRE_CMD ()
 # Description: Checks to see if the script is being run as root, and if not then exit.
 #
 # Usage: REQUIRE_ROOT
-REQUIRE_ROOT ()
-{
-	if [[ $EUID -ne 0 ]]
-	then
+# Returns: string
+REQUIRE_ROOT() {
+	if [[ $EUID -ne 0 ]]; then
 		printf "%b\n" "This script must be run as root"
 		exit 1
 	fi
+}
+
+# Description: Checks to see if the script is being run as root, and if so then exit.
+#
+# Usage: DISABLE_ROOT
+# Returns: string
+DISABLE_ROOT() {
+	if [[ $EUID -eq 0 ]]; then
+
+		PROGRAM_NAME="$(basename "$0")"
+
+		PRINT "'${PROGRAM_NAME}' should not be run as root. Please try again as a normal user."
+		exit 1
+	fi
+}
+
+# Description: Check to see if command exists
+#
+# Usage: CMD_EXISTS <command>
+# Returns: return code
+CMD_EXISTS() {
+	command -v "${1}" >/dev/null 2>&1
+	return $?
+}
+
+# Description: Check to see if input is 'yes' or empty
+#
+# Usage: CHECK_YES <var>
+# Returns: return code
+CHECK_YES() {
+	[[ $1 =~ [yY][eE]?[sS]? ]] && return 0
+	[[ -z "$1" ]] && return 0
+	return 1
 }
