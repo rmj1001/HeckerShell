5da0688976bad11865434a5086bf0ed2ad18eb80
Roy Conn <rmichael1001@gmail.com>
2021-12-02 16:00:42 -0500

    fedora postinstall script can now be curl'd

--- files/System32/postinstalls/fedora.sh
+++ files/System32/postinstalls/fedora.sh
@@ -11,71 +11,52 @@
 #		Postinstall script for fedora
 ##############################################
 
-# shellcheck disable=SC1091
-source "${SCRIPTS:=$HOME/.local/bin}"/00-api.sh
+if [[ $EUID -eq 0 ]]; then
 
-# Preprocessing flags
-DISABLE_ROOT
-REQUIRE_CMD "dnf" || exit 1
+	PROGRAM_NAME="$(basename "$0")"
+
+	printf '%b' "'${PROGRAM_NAME}' should not be run as root. "
+	printf '%b\n' "Please try again as a normal user."
+	exit 1
+fi
+
+[[ -x "$(which dnf)" ]] || exit 1
 
 ####################################
 
-REBOOT=0
-
-_help ()
-{
-	_flags ()
-	{
-		PRINT "-------------|------|---------------------|"
-		PRINT "Flag|Args|Description"
-		PRINT "-------------|------|---------------------|"
-		PRINT "|||"
-		PRINT "-r, --repos|n/a|Install common repositories"
-		PRINT "-s, --software|n/a|Install common software"
-		PRINT "-c, --config|n/a|Apply common configurations"
-		PRINT "-a, --all|n/a|Do all the things"
-		PRINT "--reboot|n/a|Reboot the computer after installation"
-		PRINT "|||"
-		PRINT "-h, --help|n/a|Show this prompt"
-	}
-
-	PRINT "$(SCRIPTNAME) - Install common fedora software"
-	PRINT "Note: it's recommended to install repositories, software, and configs in that order."
-	PRINT
-	PRINT "Usage:\t\t$(SCRIPTNAME) <flag> <args?>"
-	PRINT "Example:\t$(SCRIPTNAME) --help"
-	PRINT
-	_flags | column -t -s'|'
-}
-
-_repos ()
-{
+_repos() {
 	# RPM Fusion
 	sudo dnf install \
-		https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+		"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+		"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 	sudo dnf groupupdate core
 
-	# Flathub
-	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	# Flatpak Repositories (user-level only)
+	flatpak remote-add --user --if-not-exists \
+		flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --user --if-not-exists \
+		appcenter https://flatpak.elementary.io/repo.flatpakrepo
 }
 
-_software ()
-{
+_software() {
 	# Development
 	sudo dnf update
 	sudo dnf groupinstall "Development Tools" "Development Libraries"
 
 	# Homebrew
-	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	bash -c "$(
+		curl -fsSL
+		https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+	)"
 
 	# Gaming
 	sudo dnf install lutris
 
 	# Multimedia codecs & libdvdcss
-	sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+	sudo dnf groupupdate multimedia --setop="install_weak_deps=False" \
+		--exclude=PackageKit-gstreamer-plugin
+
 	sudo dnf groupupdate sound-and-video
 	sudo dnf install rpmfusion-free-release-tainted
 	sudo dnf install libdvdcss
@@ -88,73 +69,102 @@ _software ()
 	sudo dnf install xclip cronie
 
 	# Zap Appimage PM
-	curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh | bash -s
+	curl https://raw.githubusercontent.com/srevinsaju/zap/main/install.sh |
+		bash -s
+
 	zap init
 	zap daemon --install
 
 	# Common flatpaks
 	flatpak install --user --noninteractive --or-update flathub \
-		org.cvfosammmm.Setzer \
-		org.gnome.seahorse.Application \
-		io.gdevs.GDLauncher \
-		org.gnome.TextEditor \
-		org.gnome.Polari \
-		org.gnome.Podcasts \
-		org.gnome.DejaDup \
-		org.gnome.Builder \
-		org.gnome.Extensions \
-		io.github.seadve.Kooha \
-		com.github.maoschanz.drawing \
-		com.github.bleakgrey.tootle \
-		de.haeckerfelix.Fragments \
-		org.gimp.GIMP \
-		org.inkscape.Inkscape \
-		org.libreoffice.LibreOffice \
-		com.valvesoftware.Steam \
+		com.axosoft.GitKraken \
+		com.belmoussaoui.Authenticator \
+		com.belmoussaoui.Obfuscate \
 		com.discordapp.Discord \
+		com.etlegacy.ETLegacy \
+		com.github.bleakgrey.tootle \
+		com.github.hugolabe.Wike \
+		com.github.maoschanz.drawing \
+		com.github.rafostar.Clapper \
+		com.github.tchx84.Flatseal \
+		com.github.wwmm.easyeffects \
+		com.valvesoftware.Steam \
+		de.haeckerfelix.Fragments \
+		io.gdevs.GDLauncher \
+		io.github.achetagames.epic_asset_manager \
+		io.github.seadve.Kooha \
+		io.github.shiftey.Desktop \
+		io.mrarm.mcpelauncher \
+		net.veloren.veloren \
+		network.loki.Session \
+		nl.hjdskes.gcolor3 \
+		org.freedesktop.LinuxAudio.Plugins.LSP \
+		org.freedesktop.LinuxAudio.Plugins.TAP \
+		org.freedesktop.LinuxAudio.Plugins.ZamPlugins \
+		org.freedesktop.LinuxAudio.Plugins.swh \
+		org.freedesktop.Platform.Compat.i386 \
+		org.freedesktop.Platform.GL.default \
+		org.freedesktop.Platform.GL.default \
+		org.freedesktop.Platform.GL32.default \
+		org.freedesktop.Platform.VAAPI.Intel \
+		org.freedesktop.Platform.VAAPI.Intel \
+		org.freedesktop.Platform.VAAPI.Intel.i386 \
+		org.freedesktop.Platform.ffmpeg-full \
+		org.freedesktop.Platform.openh264 \
+		org.gimp.GIMP \
+		org.gimp.GIMP.Manual \
+		org.gnome.Boxes \
+		org.gnome.Boxes.Extension.OsinfoDb \
+		org.gnome.Builder \
+		org.gnome.Connections \
+		org.gnome.DejaDup \
+		org.gnome.Extensions \
+		org.gnome.Lollypop \
+		org.gnome.Platform \
+		org.gnome.Podcasts \
+		org.gnome.Polari \
+		org.gnome.TextEditor \
+		org.gnome.World.PikaBackup \
+		org.gnome.seahorse.Application \
+		org.gustavoperedo.FontDownloader \
+		org.inkscape.Inkscape \
+		org.kde.KStyle.Adwaita \
+		org.kde.Platform \
+		org.kde.PlatformTheme.QGnomePlatform \
+		org.kde.PlatformTheme.QtSNI \
+		org.kde.WaylandDecoration.QGnomePlatform-decoration \
+		org.kde.kdenlive \
+		org.libreoffice.LibreOffice \
+		org.mozilla.Thunderbird.Locale \
+		org.mozilla.firefox \
+		org.onlyoffice.desktopeditors \
+		org.signal.Signal \
 		org.videolan.VLC \
-		org.videolan.VLC.Plugin.makemkv \
-		org.videolan.VLC.Plugin.fdkaac
+		org.x.Warpinator \
+		re.sonny.Commit \
+		re.sonny.Tangram
 }
 
-_configs ()
-{
+_configs() {
 	# DNF
-	PRINT "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
-	PRINT "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
-	PRINT "deltarpm=True" | sudo tee -a /etc/dnf/dnf.conf
-	PRINT "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
+
+	local configFileContents="fastestmirror=True
+	max_parallel_downloads=20
+	deltrarpm=True
+	defaultyes=True"
+
+	printf '%b\n' "${configFileContents}"
 
 	# Enable cronie
 	systemctl enable crond
 }
 
-_all ()
-{
-	_repos; _development; _games; _media; _snapd; _configs
-}
+_repos
+_development
+_games
+_media
+_snapd
+_configs
 
-# If no arguments are give, just show help prompt.
-[[ $# -eq 0 ]] && _help && exit 0
-
-# Iterate over all arguments and evaluate them
-while test $# -gt 0; do
-
-	case "$(LOWERCASE ${1})" in
-
-		-r | --repos ) shift; _repos ;;
-		-s | --software ) shift; _software ;;
-		-c | --config ) shift; _configs ;;
-		-a | --all ) shift; _all; exit ;;
-		--reboot ) REBOOT=1 ;;
-
-		\? | -h | --help ) shift; _help; exit 0 ;;
-
-		* ) PRINT "$(SCRIPTNAME): Invalid argument '${1}'" && exit 1 ;;
-
-	esac
-
-done
-
-[[ ${REBOOT} -eq 1 ]] && sudo reboot
-
+printf '%b' "Reboot? (Y/n) " && read -r reboot
+[[ -z "${reboot}" || "${reboot}" =~ ^[yY][eE]?[sS]?$ ]] && systemctl reboot
