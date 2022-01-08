61a4e09be122e41609f55c06490352b7d8477105
Roy Conn <rmichael1001@gmail.com>
2022-01-07 19:02:55 -0500

    fedora script now installs vs code

--- files/System32/postinstalls/fedora.sh
+++ files/System32/postinstalls/fedora.sh
@@ -54,6 +54,12 @@ sudo dnf groupupdate core
 sudo dnf update
 sudo dnf groupinstall "Development Tools" "Development Libraries"
 
+# Development: VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
 # Multimedia codecs & libdvdcss
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" \
 	--exclude=PackageKit-gstreamer-plugin
