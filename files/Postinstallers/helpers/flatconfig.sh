7e912dfe2e216c9f3e113273e52eb6e881862621
Roy Conn <rmichael1001@gmail.com>
2022-02-04 12:53:08 -0500

    updated flatpak list

--- files/Postinstallers/helpers/flatconfig.sh
+++ files/Postinstallers/helpers/flatconfig.sh
@@ -24,7 +24,9 @@ flatpak install --user --noninteractive --or-update flathub \
 	com.axosoft.GitKraken \
 	com.belmoussaoui.Authenticator \
 	com.belmoussaoui.Obfuscate \
-	com.discordapp.Discord \
+	com.belmoussaoui.ashpd.demo \
+	com.bitstower.Markets \
+	com.bitwarden.desktop \
 	com.etlegacy.ETLegacy \
 	com.github.bleakgrey.tootle \
 	com.github.hugolabe.Wike \
@@ -33,58 +35,77 @@ flatpak install --user --noninteractive --or-update flathub \
 	com.github.tchx84.Flatseal \
 	com.github.wwmm.easyeffects \
 	com.gitlab.newsflash \
+	com.mattjakeman.ExtensionManager \
+	com.obsproject.Studio \
+	com.rafaelmardojai.Blanket \
+	com.skype.Client \
 	com.thebrokenrail.MCPIReborn \
 	com.usebottles.bottles \
-	com.valvesoftware.Steam \
+	com.vscodium.codium \
 	de.haeckerfelix.Fragments \
 	de.haeckerfelix.Shortwave \
 	dev.geopjr.Hashbrown \
 	fr.romainvigier.MetadataCleaner \
+	im.bernard.Nostalgia \
 	io.github.Soundux \
 	io.github.achetagames.epic_asset_manager \
 	io.github.prateekmedia.appimagepool \
 	io.github.seadve.Kooha \
 	io.github.shiftey.Desktop \
-	io.mrarm.mcpelauncher \
 	net.veloren.veloren \
 	network.loki.Session \
 	nl.hjdskes.gcolor3 \
+	org.freedesktop.Sdk \
+	org.freedesktop.Sdk.Extension.dotnet \
+	org.freedesktop.Sdk.Extension.golang \
+	org.freedesktop.Sdk.Extension.haskell \
+	org.freedesktop.Sdk.Extension.mono6 \
+	org.freedesktop.Sdk.Extension.node16 \
+	org.freedesktop.Sdk.Extension.openjdk \
+	org.freedesktop.Sdk.Extension.php74 \
+	org.freedesktop.Sdk.Extension.rust-stable \
+	org.gabmus.giara \
 	org.gimp.GIMP \
 	org.gimp.GIMP.Manual \
+	org.gimp.GIMP.Plugin.Resynthesizer \
+	org.gnome.Aisleriot \
 	org.gnome.Boxes \
 	org.gnome.Boxes.Extension.OsinfoDb \
 	org.gnome.Builder \
 	org.gnome.Calculator \
+	org.gnome.Calls \
 	org.gnome.Chess \
 	org.gnome.Connections \
 	org.gnome.DejaDup \
 	org.gnome.Evince \
 	org.gnome.Extensions \
+	org.gnome.Fractal \
 	org.gnome.Lollypop \
 	org.gnome.Maps \
 	org.gnome.Mines \
+	org.gnome.PasswordSafe \
 	org.gnome.Podcasts \
 	org.gnome.Polari \
-	org.gnome.Sdk \
 	org.gnome.TextEditor \
 	org.gnome.World.PikaBackup \
 	org.gnome.design.IconLibrary \
 	org.gnome.eog \
+	org.gnome.gitlab.Cowsay \
 	org.gnome.gitlab.YaLTeR.VideoTrimmer \
-	org.gnome.gitlab.somas.Apostrophe \
 	org.gnome.gitlab.somas.Apostrophe \
 	org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive \
 	org.gnome.seahorse.Application \
 	org.gnucash.GnuCash \
-	org.gnucash.GnuCash.Locale \
 	org.gustavoperedo.FontDownloader \
 	org.inkscape.Inkscape \
 	org.kde.kdenlive \
+	org.kde.krita \
 	org.libreoffice.LibreOffice \
 	org.mozilla.Thunderbird.Locale \
 	org.mozilla.firefox \
 	org.onlyoffice.desktopeditors \
 	org.signal.Signal \
+	org.turbowarp.TurboWarp \
 	org.videolan.VLC \
 	org.x.Warpinator \
 	org.xonotic.Xonotic \
