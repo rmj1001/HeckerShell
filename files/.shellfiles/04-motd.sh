a9725b78f4a59e722ce1c09a0daa701e8a99bdd5
Roy Conn <rmichael1001@gmail.com>
2022-02-07 15:32:37 -0500

    moved motd to separate file for easier editing

---
+++ files/.shellfiles/04-motd.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+### Print custom MOTD to terminal
+# Usage: motd
+function motd() {
+    PRINT "########################################################"
+    PRINT "#"
+    PRINT "# Roy Conn"
+    PRINT "#"
+    PRINT "# Reddit \t u/rmj1001"
+    PRINT "# Twitter \t @RoyConn10"
+    PRINT "# Matrix \t @rmj1001:matrix.org"
+    PRINT "# Mastodon \t @Hydra_Slash_Linux@foostodon.org"
+    PRINT "# YouTube \t https://bit.ly/3qGsGSJ"
+    PRINT "# Github \t https://github.com/rmj"
+    PRINT "#"
+    PRINT "########################################################"
+}
