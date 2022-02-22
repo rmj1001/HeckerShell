f860109fea7dcb3ad75d4341f4311d626f182e54
Roy Conn <rmichael1001@gmail.com>
2022-02-22 08:55:55 -0500

    Papirus Icons & folder colors and settings change

--- files/Postinstallers/helpers/portableApps.sh
+++ files/Postinstallers/helpers/portableApps.sh
@@ -23,3 +23,7 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/in
 
 # Papirus Icons
 wget -qO- https://git.io/papirus-icon-theme-install | sh
+wget -qO- https://git.io/papirus-folders-install | sh
+
+papirus-folders --theme Papirus-Dark -C yaru
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
