001eb04638e2235299931d893e63c19c870f5e62
Roy Conn <rmichael1001@gmail.com>
2022-02-22 09:15:08 -0500

    Papirus-Light

--- files/Postinstallers/helpers/portableApps.sh
+++ files/Postinstallers/helpers/portableApps.sh
@@ -26,4 +26,5 @@ wget -qO- https://git.io/papirus-icon-theme-install | sh
 wget -qO- https://git.io/papirus-folders-install | sh
 
 papirus-folders --theme Papirus-Dark -C yaru
-gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+papirus-folders --theme Papirus-Light -C yaru
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Light
