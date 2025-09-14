485c7b15ee1f215e1095e7e4c399d68302951439
Tucker's Kobold <rmichael1001@gmail.com>
2025-09-14 15:46:05 -0400

    add variables file

---
+++ auto/variables.sh
@@ -0,0 +1,21 @@
+# HeckerShell urls
+export HECKERSHELL_SITE_HTTPS="https://github.com/rmj1001/HeckerShell.git"
+export HECKERSHELL_SITE_SSH="git@github.com:rmj1001/HeckerShell.git"
+export HECKERSHELL_SITE="${HECKERSHELL_SITE_HTTPS}"
+
+# HeckerShell directories
+export HECKERSHELL_DOWN_DIR="${HOME}/.local/share"
+export HECKERSHELL_DIR="${HECKERSHELL_DOWN_DIR}/HeckerShell"
+export HECKERSHELL="${HECKERSHELL_DIR}/files"
+
+# OG Paths
+export SYM_ZSHRC="${HECKERSHELL}/.zshrc"
+export SYM_BASHRC="${HECKERSHELL}/.bashrc"
+export SYM_SHELLFILES="${HECKERSHELL}/.shellfiles"
+export SYM_SCRIPTS="${HECKERSHELL}/Scripts"
+
+# Paths
+export ZSHRC="${HOME}/.zshrc"
+export BASHRC="${HOME}/.bashrc"
+export SHELLFILES="${HOME}/.shellfiles"
+export SCRIPTS="${HOME}/Scripts"
